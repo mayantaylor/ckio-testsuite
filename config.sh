c4597947347401041593d939bbd493393dc6eb4f
Maya Taylor <mayat4@login07.frontier.olcf.ornl.gov>
2024-05-17 15:12:11 -0400

    cleanup file generation

---
+++ config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export TESTFILE_DIR=/lustre/orion/bip249/scratch/mayat4/
+export CHARM_ROOT=/ccs/home/mayat4/software/charm/bin/
--- writer.py
+++ writer.py
@@ -1,18 +1,28 @@
 import sys
 import argparse
 
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
 GB = 1024*1024*1024
 MB = 1024 * 1024 
-filename = sys.argv[1]
 
+base_filename = sys.argv[1] + "/testfile"
 filesize = int(float(sys.argv[2]) * MB)
+count = int(sys.argv[3])
 
-# Create 4KB of data
-data = b"a" * 2048
+for i in range(0, count):
+    amode = MPI.MODE_WRONLY|MPI.MODE_CREATE
+    filename = base_filename + "_" + str(rank) + "_" + str(i) + ".txt"
 
-print(f'Writing file {filename} of size {filesize / (1024*1024*1024)}GB with data {data[0:2]}')
-with open(filename, 'wb') as f:
-    for _ in range(filesize // len(data)):
-        f.write(data)
+    # Create 4KB of data
+    data = b"a" * 2048
+
+    with open(filename, 'wb') as f:
+        for _ in range(filesize // len(data)):
+            f.write(data)
 
 
